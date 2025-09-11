f37770083418ebe860f957630502696c627092dc
Martin Doulík <123932205+robofancz@users.noreply.github.com>
2025-09-11 12:17:34 +0200

    Update first.php

--- I2B/PHP/first.php
+++ I2B/PHP/first.php
@@ -1,1 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prvni</title>
+    <link rel="stylesheet" href="../css/base.css">
+</head>
+<body>
+    <h1>úvod do php</h1>
 
+    <?php
+        echo "<u>Hello World!</u>";
+    ?>
+
+    <P>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam officia necessitatibus maiores laudantium porro doloremque corporis beatae repellat blanditiis, doloribus nostrum dolor, dicta, odit ratione molestiae maxime nesciunt ipsa voluptate.
+    </P>
+    
+</body>
+</html>
