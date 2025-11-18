df867c278e804518f38be2748f8b9e6514bc1dcb
Martin Doulík <123932205+robofancz@users.noreply.github.com>
2025-11-18 11:26:45 +0100

    Add files via upload

--- I2B/PHP/vetveni.php
+++ I2B/PHP/vetveni.php
@@ -50,7 +50,32 @@
         echo("Proměnná číslo je záporné číslo");
     }
 ?>
-
+<p>
+    Pomocí jen jednoho větvení rozdělte čísla na kladná, záporná, sudá a lichá. Nulu ošetřete v samostatné větvi. Pro každou kategorii vypište hodnotu proměnné a o jakou skupinu čísel se jedná.
+</p>
+<?php
+    $cislo = -7;
+    if ($cislo<0 && $cislo%2==0)
+    {
+        echo ($cislo . " je sudé a záporné");
+    }
+    elseif($cislo<0 && $cislo%2!=0)
+    {
+       echo ($cislo . " je liché a záporné");
+    }
+    elseif ($cislo> 0&& $cislo%2==0)
+    {
+        echo ($cislo . " je sudé a kladné");
+    }
+    elseif ($cislo>0 && $cislo%2!=0)
+    {
+        echo ($cislo . " je liché a kladné");
+    }
+    else
+    {
+        echo ($cislo . "je 0");
+    }
+?>
 
 </body>
 </html>
