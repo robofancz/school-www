099fc6c5b56d8cb877c62173f4a934f2139bd10e
Martin Doulík <123932205+robofancz@users.noreply.github.com>
2025-11-25 11:27:53 +0100

    Add files via upload

---
+++ I2B/PHP/switch.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Switch</title>
+    <link rel="stylesheet" href="../css/base.css">
+</head>
+<body>
+    
+<h1>Větvení Programu</h1>
+<h2>Větvení pomocí switch</h2>
+<p>
+    Jedná se o jednoduché větvení které lze použít za specifických podmínek: 
+</p>
+<li>
+    <ul>
+        procujeme jen s jednou proměnnou
+    </ul>
+    <ul>
+        nesmí se jednat o interval ale o konkrétní hodnoty
+    </ul>
+</li>
+<p>
+    Lze ho vždy nahradit větvením if .. else
+</p>
+<p>
+    Umožňujenám vyhnout se složitým podmínkám
+</p>
+
+<?php
+    /*
+        Obecný zápis:
+
+        switch($promenna)
+        {
+            case 'hodnota1':
+                příkaz;
+                .
+                .
+                .
+                break;
+
+            case 'hodnota2':
+                příkaz;
+                .
+                .
+                .
+                break;
+            .
+            .
+            .
+            default: 
+            //provede se tehdy když není splněna žádná další varianta
+                příkaz;
+        }
+    */
+?>
+
+
+
+
+
+
+</body>
+</html>
