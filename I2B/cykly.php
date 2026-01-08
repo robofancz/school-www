b90faa01f343c7588fca7c179d5ce5465fff4876
Martin Doulík <123932205+robofancz@users.noreply.github.com>
2026-01-08 12:18:23 +0100

    Add files via upload

--- I2B/cykly.php
+++ I2B/cykly.php
@@ -70,11 +70,98 @@
             }
         }
 
-        echo("<br><hr>")
-        //
+        echo("<br><hr>");
+        //Pokud je hodnota proměnné $e kladná, snižujte ji postupně k nuke a hodnoty vyište zeleně. Pokud je ale hodnota proměnné záporná, zvyšujte je k nule a hodnoty vypište červeně. Pokud je hodnotou nula vypište ji černě.
+        $e = 10;
+        if ($e>0) {
+            for ($e; $e >= 0; $e--) { 
+                echo ("<span style='color: green'> $e |</span>");
+            }
+        }
+        elseif ($e<0) {
+            for ($e; $e <= 0; $e++) { 
+                echo ("<span style='color: red'> $e | </span>");
+            }
+        }
+        else {
+            echo ($e);
+        }
         
-        
+
+        echo("<br><hr>");
+
     ?>
+    <h3>Cyklus WHILE</h3>
+    <?php
+        /*
+        Teoretický zápis:
+
+        start;
+        while(podmínka)
+        {
+            Příkazy; //Provede se, pokud je podmínka splněna
+
+            změna;
+        }
+        */
+
+        //Pomocí cyklu WHILE vypište hodnoty proměnně $f od -10 do 10
+        $f=-10;
+        while ($f <= 10) {
+            echo ($f . " | ");
+            $f++;
+        }
+
+        echo("<br><hr>");
+
+        //Pomocí cyklu WHILE vypište hodnoty proměnné $g od -6 do 8, změna při jednom průchodu cyklem je 0,1.
+        $g=-6;
+        while ($g <= 8) {
+            $g = round($g,2);
+            echo ($g ." | ");
+            $g=$g+0.1;
+        }
+
+
+        echo("<br><hr>");
+
+        //Zjistěte, kolik hodnot je na vypsáno v minulé úloze
+        $h=-6;
+        $count=0;
+        while ($h <= 8) {
+            $h = round($h,2);
+            echo ($h ." | ");
+            $h=$h+0.1;
+            $count++;
+        }
+
+        echo ("<br><br>" . $count);
+
+        echo("<br><hr>");
+
+    ?>
+
+    <h2>Cykly kontrolující podmínku na konci</h2>
+    <p>
+        Tento cyklus proběhne vždy alespoň jednou, bez ohledu na to, jestli byla podmínka splněna. Toho se využívá například pro naplnění chybových hlášek nebo proměnných s jiným obsahem.
+    </p>
+    <h3>Cyklus DO WHILE</h3>
+    <?php
+        /*
+        Teoretický zápis:
+
+        start;
+        do
+        {
+            příkazy;    //provede se vždy alespoň jednou
+
+            změna;
+        }
+        while(podmínka)
+        */
+    ?>
+
+    
     
 </body>
 </html>
